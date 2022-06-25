3ff9dc6e9014fb43714a8428f29a4864ca77793a
Cairo Campos <cairocampos98@gmail.com>
2022-06-25 11:43:59 -0300

    add cogeden graphql

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,49 +1,20 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
-import { useQuery, gql } from "@apollo/client";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
 import "@vime/core/themes/default.css";
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
   lessonSlug: string;
 }
 
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug ($slug: String) {
-    lesson(where: {slug: $slug}) {
-      id
-      title
-      description
-      videoId
-      teacher {
-        name
-        bio
-        avatarURL
-      }
-    }
-  }
-`
-
-interface GetLessonBySlugResponse {
-  lesson: {
-    id:string;
-    title:string;
-    description:string;
-    videoId:string;
-    teacher: {
-      name:string;
-      bio:string;
-    }
-  }
-}
-
 export function Video(props: VideoProps) {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug
     }
   });
 
-  if(!data) {
+  if(!data || !data.lesson) {
     return (
       <div className="flex-1">
         <p>Carregando...</p>
@@ -66,24 +37,26 @@ export function Video(props: VideoProps) {
 
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
-          <div className="flex-1">
-            <h1 className="text-2xl font-bold">{data?.lesson.title}</h1>
-            <p className="mt-4 text-gray-200 leading-relaxed">
-              {data?.lesson.description}
-            </p>
+          {data.lesson.teacher && (
+            <div className="flex-1">
+              <h1 className="text-2xl font-bold">{data?.lesson.title}</h1>
+              <p className="mt-4 text-gray-200 leading-relaxed">
+                {data?.lesson.description}
+              </p>
 
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/diego3g.png"
-                alt=""
-              />
-              <div className="leading-relaxed">
-                <strong className="text-lg">{data?.lesson.teacher.name}</strong>
-                <span className="text-sm text-gray-200 block">{data?.lesson.teacher.bio}</span>
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-blue-500"
+                  src="https://github.com/diego3g.png"
+                  alt=""
+                />
+                <div className="leading-relaxed">
+                  <strong className="text-lg">{data?.lesson.teacher.name}</strong>
+                  <span className="text-sm text-gray-200 block">{data?.lesson.teacher.bio}</span>
+                </div>
               </div>
             </div>
-          </div>
+          )}
           <div className="flex flex-col gap-4">
             <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
               <DiscordLogo size={24}/>
